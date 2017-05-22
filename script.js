78a60cde1354c5bd5a718b917bb7457e4b3c3775
Amy Boudsady <aeimskei@gmail.com>
2017-05-22 22:39:31 +0000

    Stop the timer when the test is done

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-// Helper function
 function leadingZero(time) {
     if (time <= 9) {
         time = "0" + time;
     }
     return time;
 }
-
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
@@ -27,29 +27,40 @@ function runTimer() {
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
-
 // Match the text entered with the provided text on the page:
-    function spellCheck() {
-        let textEntered = testArea.value;
-        console.log(textEntered);
+function spellCheck() {
+    let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0,textEntered.length);
+
+    if (textEntered == originText) {
+        clearInterval(interval);
+        testWrapper.style.borderColor = "#429890";
+    } else {
+        if (textEntered == originTextMatch) {
+            testWrapper.style.borderColor = "#65CCf3";
+        } else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
     }
+
+}
 
 // Start the timer:
-    function start() {
-        let textEnteredLength = testArea.value.length;
-        if (textEnteredLength === 0) {
-            setInterval(runTimer, 10);
-        }
-        console.log(textEnteredLength);
+function start() {
+    let textEnterdLength = testArea.value.length;
+    if (textEnterdLength === 0 && !timerRunning) {
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
     }
+    console.log(textEnterdLength);
+}
 
 // Reset everything:
-    function reset() {
-        console.log("reset button has been pressed!")
-    }
+function reset() {
+    console.log("reset button has been pressed!");
+}
 
-
-// Event listeners for keyboard input and the reset button:
-    testArea.addEventListener("keypress", start, false);
-    testArea.addEventListener("keyup", spellCheck, false);
-    resetButton.addEventListener("click", reset, false);
+// Event listeners for keyboard input and the reset
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
